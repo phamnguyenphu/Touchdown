be5712bc4c0e8a492ac55e825ec76d5f363b29e5
Pham Nguyen Phu <57014052+phamnguyenphu@users.noreply.github.com>
2023-04-05 13:30:54 +0700

    Set up the Touchdown IOS project in XCODE

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,28 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Pham Nguyen Phu on 05/04/2023.
+//
+
+import SwiftUI
+
+// DATA
+
+// COLOR
+let colorBackground: Color = .init("ColorBackground")
+let colorGray: Color = Color(.systemGray4)
+
+// LAYOUT
+
+// UX
+
+// API
+
+// IMAGE
+
+// FONT
+
+// STRING
+
+// MISC
