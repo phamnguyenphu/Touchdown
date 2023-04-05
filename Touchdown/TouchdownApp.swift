62759501404efb18ecb54ca1c9c0db98b6871fd5
Pham Nguyen Phu <57014052+phamnguyenphu@users.noreply.github.com>
2023-04-05 12:57:52 +0700

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Pham Nguyen Phu on 05/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
