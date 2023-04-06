d7780a9e8ac43829b9b1755ab252f04e54e0009d
Pham Nguyen Phu <57014052+phamnguyenphu@users.noreply.github.com>
2023-04-06 09:41:26 +0700

    Update NavigationBarView.swift

--- Touchdown/View/Home/NavigationBarView.swift
+++ Touchdown/View/Home/NavigationBarView.swift
@@ -16,34 +16,38 @@ struct NavigationBarView: View {
 
     var body: some View {
         HStack {
-            Button {} label: {
+            Button {
+                print("search")
+            } label: {
                 Image(systemName: "magnifyingglass")
                     .font(.title)
                     .foregroundColor(.black)
+            }
 
-                Spacer()
+            Spacer()
 
-                LogoView()
-                    .opacity(isAnimating ? 1 : 0)
-                    .offset(y: isAnimating ? 0 : -25)
-                    .onAppear {
-                        withAnimation(.easeOut(duration: 0.75)) {
-                            isAnimating.toggle()
-                        }
+            LogoView()
+                .opacity(isAnimating ? 1 : 0)
+                .offset(y: isAnimating ? 0 : -25)
+                .onAppear {
+                    withAnimation(.easeOut(duration: 0.75)) {
+                        isAnimating = true
                     }
+                }
 
-                Spacer()
+            Spacer()
 
-                Button {} label: {
-                    ZStack {
-                        Image(systemName: "cart")
-                            .font(.title)
-                            .foregroundColor(.black)
-                        Circle()
-                            .fill(.red)
-                            .frame(width: 14, height: 14, alignment: .center)
-                            .offset(x: 13, y: -10)
-                    }
+            Button {
+                print("cart")
+            } label: {
+                ZStack {
+                    Image(systemName: "cart")
+                        .font(.title)
+                        .foregroundColor(.black)
+                    Circle()
+                        .fill(.red)
+                        .frame(width: 14, height: 14, alignment: .center)
+                        .offset(x: 13, y: -10)
                 }
             }
         } //: HSTACK
